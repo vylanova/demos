8a92f569d2f3b088fb896f91a6eb30b9d03c746b
Evandro vila <vylanova@gmail.com>
2018-05-22 14:46:14 -0300

    master

---
+++ src/main/java/br/com/russas/ufc/n2s/App.java
@@ -0,0 +1,13 @@
+package br.com.russas.ufc.n2s;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
